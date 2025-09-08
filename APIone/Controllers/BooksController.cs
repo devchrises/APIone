becf12cfcf81baf521429ce822e61c836acee4f2
Chrises <146405590+devchrises@users.noreply.github.com>
2025-09-08 04:49:58 -0500

    feat(BookController): add CRUD operations for books

--- APIone/Controllers/BooksController.cs
+++ APIone/Controllers/BooksController.cs
@@ -5,7 +5,6 @@ namespace APIone.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-
     public class BooksController : ControllerBase
     {
         static private List<Book> books = new List<Book>
@@ -51,6 +50,54 @@ namespace APIone.Controllers
         public ActionResult<List<Book>> GetBooks()
         {
             return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book newBook)
+        {
+            if (newBook == null)
+            {
+                return BadRequest();
+            }
+
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book updateBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null) return NotFound();
+
+            book.Id = updateBook.Id;
+            book.Author = updateBook.Author;
+            book.Title = updateBook.Title;
+            book.YearPublished = updateBook.YearPublished;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null) return NotFound();
+            
+            books.Remove(book);
+            return NoContent();
         }
     }
 }
