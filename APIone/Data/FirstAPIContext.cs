863061fdbbf569c6c447a701c605e4bf67cb09d4
Chrises <146405590+devchrises@users.noreply.github.com>
2025-09-08 20:55:17 -0500

    feat(db): add FirstAPIContext, migrations and seed data
    Adds DbContext, EF Core Migrations and initial seed data.

---
+++ APIone/Data/FirstAPIContext.cs
@@ -0,0 +1,53 @@
+﻿using APIone.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIone.Data
+{
+    public class FirstAPIContext : DbContext
+    {
+        public FirstAPIContext(DbContextOptions<FirstAPIContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Book>().HasData(
+            new Book
+            {
+                Id = 1,
+                Author = "A1",
+                Title = "T1",
+                YearPublished = 2001
+            },
+            new Book
+            {
+                Id = 2,
+                Author = "A2",
+                Title = "T2",
+                YearPublished = 2002
+            },
+            new Book
+            {
+                Id = 3,
+                Author = "A3",
+                Title = "T3",
+                YearPublished = 2003
+            },
+            new Book
+            {
+                Id = 4,
+                Author = "A4",
+                Title = "T4",
+                YearPublished = 2004
+            },
+            new Book
+            {
+                Id = 5,
+                Author = "A5",
+                Title = "T5",
+                YearPublished = 2005
+            }
+            );
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ APIone/Migrations/20250908120659_book data added.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace APIone.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookdataadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 1, "A1", "T1", 2001 },
+                    { 2, "A2", "T2", 2002 },
+                    { 3, "A3", "T3", 2003 },
+                    { 4, "A4", "T4", 2004 },
+                    { 5, "A5", "T5", 2005 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
